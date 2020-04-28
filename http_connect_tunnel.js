237fbe9269cbdccbc32fcbc08b48a111376e1046
Adam Malcontenti-Wilson <amalcontenti-wilson@zendesk.com>
2020-04-29 01:37:17 +1000

    Support HTTP basic authentication

--- http_connect_tunnel.js
+++ http_connect_tunnel.js
@@ -2,13 +2,22 @@ var debug = require('debug')('snitunnel:http-connect-tunnel');
 var http = require('http');
 
 module.exports = {
-  connect: function(port, host, tunnelPort, tunnelHost, cb) {
-    var req = http.request({
-      host: tunnelHost,
-      port: tunnelPort,
+  connect: function(port, host, tunnelUrl, cb) {
+    var tunnel = new URL(tunnelUrl);
+    var requestParams = {
+      headers: {},
+      host: tunnel.host,
+      port: tunnel.port || 80,
       method: 'CONNECT',
       path: `${host}:${port}`
-    });
+    };
+
+    if (tunnel.username) {
+      var auth = `${tunnel.username}:${tunnel.password}`
+      requestParams.headers['Proxy-Authorization'] = `Basic ${Buffer.from(auth).toString('base64')}`
+    }
+
+    var req = http.request(requestParams);
     req.on('connect', cb);
     req.on('response', cb);
     req.end();
--- sni_proxy_app.js
+++ sni_proxy_app.js
@@ -24,8 +24,7 @@ function connectionHandler(req, serverSocket, header, handshake) {
     debug('Using %s tunnel for connection to %s', tunnel, req.serverName);
 
     if (tunnel.indexOf('http://') == 0) {
-      var match = /^http:\/\/(.+?):(\d+)/.exec(tunnel);
-      httpConnectTunnel.connect(443, req.serverName, match[2], match[1], function(res, clientSocket, head) {
+      httpConnectTunnel.connect(443, req.serverName, tunnel, function(res, clientSocket, head) {
         debug('HTTP Tunnel Status Code %d %s', res.statusCode, res.statusMessage);
         if (clientSocket) {
           connectionHandler(clientSocket, head);
